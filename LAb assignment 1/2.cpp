4fc2d9852d54308c9e67e665c87272bc21fc1e45
harshit <hperiwal_be24@thapar.edu>
2025-08-19 09:02:33 +0530

    Intial commit - uploaded lab assignment

---
+++ LAb assignment 1/2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+    int arr[100], n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+	cin >> arr[i];
+	}
+    
+
+}
