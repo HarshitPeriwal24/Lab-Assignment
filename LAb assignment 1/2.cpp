977b011c49b3e8d926c957fc6d0222aab56f3838
harshit <hperiwal_be24@thapar.edu>
2025-08-19 13:37:04 +0530

    files uploaded

--- LAb assignment 1/2.cpp
+++ LAb assignment 1/2.cpp
@@ -1,8 +1,18 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-
-
+int removeDuplicates(int arr[], int n) {
+    sort(arr, arr + n);
+    int j = 0;
+    for(int i = 1; i < n; i++) {
+        if(arr[i] != arr[j]) {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+    return j + 1;
+}
 int main() {
     int arr[100], n;
     cout << "Enter number of elements: ";
@@ -11,6 +21,10 @@ int main() {
     for(int i = 0; i < n; i++) {
 	cin >> arr[i];
 	}
-    
-
+    int newSize = removeDuplicates(arr, n);
+    cout << "Unique elements:\n";
+    for(int i = 0; i < newSize; i++){
+	 cout << arr[i] << " ";
+	}
+    return 0;
 }
